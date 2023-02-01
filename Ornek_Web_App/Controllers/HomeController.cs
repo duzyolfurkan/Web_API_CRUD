d28ff94d555ec557126c584da5d29c2e2e505682
Furkan Düzyol <duzyolfurkan@gmail.com>
2023-02-01 12:27:58 +0300

    Add project files.

---
+++ Ornek_Web_App/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ornek_Web_App.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ornek_Web_App.Controllers
+{
+    //Burada contributeler ile bu controller'ın api olduğu belirtilir.
+    [ApiController]
+    [Route("api/[controller]")]
+    //API Controller ControllerBase'den referans alır.
+    public class HomeController : ControllerBase
+    {
+        //[HttpGet]
+        //public string Get()
+        //{
+        //    return "Home";
+        //}
+
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "Home" + id;
+        //}
+
+        private List<User> users = Fake.FakeData.GetUsers(100);
+
+        [HttpGet] //Tüm verileri çekme
+        public List<User> GetUsers() 
+        { 
+            return users; 
+        }
+
+        [HttpGet("{id}")] //ID ye göre veri çekme
+        public User GetUsers(int id)
+        {
+            return users.FirstOrDefault(x=>x.Id == id);
+        }
+
+        //Frombody kullanıcının hangi şekilde ekleneceği yazıyor.
+        [HttpPost] //Ekleme
+        public User Post([FromBody]User user)
+        {
+            users.Add(user);
+            return user;
+        }
+
+        [HttpPut] //Update
+        public User Put([FromBody]User user)
+        {
+            var editUser = users.FirstOrDefault(x => x.Id == user.Id);
+            editUser.FirstName= user.FirstName;
+            editUser.LastName= user.LastName;
+            editUser.Address= user.Address;
+            return user;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var deletedUser = users.FirstOrDefault(x => x.Id == id);
+            users.Remove(deletedUser);
+        }
+    }
+}
---
+++ Ornek_Web_App/Fake/FakeData.cs
@@ -0,0 +1,26 @@
+﻿using Bogus;
+using Ornek_Web_App.Models;
+using System.Collections.Generic;
+
+namespace Ornek_Web_App.Fake
+{
+    public static class FakeData
+    {
+        public static List<User> _users;
+
+        public static List<User> GetUsers(int number)
+        {
+            if (_users == null)
+            {
+                _users = new Faker<User>()
+                .RuleFor(x => x.Id, f => f.IndexFaker + 1)
+                .RuleFor(x => x.FirstName, f => f.Name.FirstName())
+                .RuleFor(x => x.LastName, f => f.Name.LastName())
+                .RuleFor(x => x.Address, f => f.Address.FullAddress())
+                .Generate(number);
+            }
+
+            return _users;
+        }
+    }
+}
